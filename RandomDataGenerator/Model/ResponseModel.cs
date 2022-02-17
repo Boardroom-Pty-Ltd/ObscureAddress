51a963e46579f18da6d9bfc723e7afd7d50aa840
Khushbu Hirapara <khushbuh.sky@gmail.com>
2022-02-17 19:35:28 +0530

    Initial commit for the conversion data from original form to the un-recognizable human readable form.

---
+++ RandomDataGenerator/Model/ResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace RandomDataGenerator.Model
+{
+    public class ResponseModel
+    {
+        public List<string> PreExistingErrors { get; set; }
+        public List<string> FinalErrors { get; set; }
+        public InvestorModel Data { get; set; }
+
+        public ResponseModel()
+        { }
+    }
+}
